3e60a7372e318dab3c8166d191c79a5f541f02f4
Bhavishyauttreja <72184286+Bhavishyauttreja@users.noreply.github.com>
2022-06-18 21:27:50 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+// First Image code in JS
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "image/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+// Second Image code in JS
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomImageSource2 = "image/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+// Condition for checking the number is big or small
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML= "Player 1 Wins!"
+}
+else if(randomNumber2 < randomNumber1)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else
+{
+    document.querySelector("h1").innerHTML="Draw!";
+}
